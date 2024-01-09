8903c23d0c92fc8ecc5ef6df4e0bbcad7fdd1b32
Amir Semsarzadeh <76231011+Amirdatasci2021@users.noreply.github.com>
2024-01-09 14:30:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit 
 
 streamlit.title('My Patrents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔 ')
+streamlit.text('Avacado Toast 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

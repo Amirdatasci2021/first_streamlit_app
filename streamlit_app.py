1031b6f85018fb6cf9e97b86eebb9d4d0f5fc428
Amir Semsarzadeh <76231011+Amirdatasci2021@users.noreply.github.com>
2024-01-09 14:38:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔 ')
 streamlit.text('Avacado Toast 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
